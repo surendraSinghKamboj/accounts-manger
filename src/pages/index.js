10c19a9b109cd1ea6b954c1eca26a03357666461
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-29 18:37:37 +0530

    error fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import Link from "next/link";
 import { GiWhiteBook } from 'react-icons/gi'

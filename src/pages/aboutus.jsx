b7d3db39a5fec510308d0d5dd6912a6984967db7
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-30 16:20:03 +0530

    contact and About Pages

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import React from "react";
+import React, { useId } from "react";
 
 const aboutus = () => {
 	const navMenu = [
@@ -8,9 +8,28 @@ const aboutus = () => {
 		{ path: "/contactus", menu: "Contact us" },
 		{ path: "/Login", menu: "Login Now" },
 	];
-	return <>
-  <Navbar navMenu={navMenu}/>
-  </>;
+
+	const aboutData = [
+		"Welcome to our Accounts Manager web app! We are excited to provide you with a comprehensive solution to manage all of your sales and purchases in one place.",
+		"Our app is designed with ease of use in mind, so whether you are a seasoned accounting professional or new to managing your own finances, you will find our platform intuitive and straightforward.",
+		"With our app, you can easily track your sales and purchases, generate invoices and receipts, and manage your accounts payable and accounts receivable. You can also generate financial reports to help you make informed business decisions.",
+		"We understand the importance of security when it comes to your financial information, which is why our app utilizes the latest encryption technologies to ensure your data is always safe and secure.",
+		"Thank you for choosing our Accounts Manager web app. We are dedicated to providing you with the best possible experience, and we are always here to help if you have any questions or feedback.",
+	];
+
+	return (
+		<>
+			<Navbar navMenu={navMenu} />
+			<section className="flex justify-center m-auto items-center flex-col sm:w-10/12 w-[94%]">
+				<h3>About Us</h3>
+				{aboutData.map((item) => (
+					<div key={useId()}>
+						<p>{item}</p>
+					</div>
+				))}
+			</section>
+		</>
+	);
 };
 
 export default aboutus;
--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import React from "react";
+import React, { useId } from "react";
 
 const contactus = () => {
 	const navMenu = [
@@ -8,9 +8,25 @@ const contactus = () => {
 		{ path: "/contactus", menu: "Contact us" },
 		{ path: "/Login", menu: "Login Now" },
 	];
+
+	const contactData = [
+		"Welcome to our Accounts Manager web app! We're excited to help you streamline your sales and purchase processes. If you have any questions, concerns, or feedback, please don't hesitate to reach out to us. Our team of experienced account managers is ready to assist you with any issues you may encounter.",
+		"To get in touch, you can use the contact form below or email us at surendra.singh.kamboj@hotmail.com. We will get back to you as soon as possible.",
+		"Thank you for choosing our Accounts Manager web app. We appreciate your business and look forward to helping you manage your sales and purchase with ease!",
+	];
+
 	return (
 		<>
 			<Navbar navMenu={navMenu} />
+
+			<section className="flex justify-center m-auto items-center flex-col sm:w-10/12 w-[94%]">
+				<h3>Contact Us</h3>
+				{contactData.map((item) => (
+					<div key={useId()}>
+						<p>{item}</p>
+					</div>
+				))}
+			</section>
 		</>
 	);
 };
